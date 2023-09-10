b0366c9043e10acb300d1a290f55865cfb2dc4cb
christopherdent <christopherdent01@gmail.com>
2023-09-09 22:27:08 -0400

    Expands PetsController

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,13 +1,62 @@
 class PetsController < ApplicationController
 
-    def index
-        @pets = Pet.all
-        render json: @pets
+  ##Crud Actions
+    def create
+      @pet = Pet.new(pet_params)
+      if @pet.save
+        render json: @pet, status: :created
+      else
+        render json: @pet.errors, status: :unprocessable_entity
       end
+    end
 
-      def show_dogs
-        @dogs = Pet.by_species("Dog")
-        render json: @dogs
+    def index
+      @pets = Pet.all
+      render json: @pets
+    end
+
+    def show
+      @pet = Pet.find(params[:id])
+      render json: @pet
+    end
+
+    def update
+      @pet = Pet.find(params[:id])
+      if @pet.update(pet_params)
+        render json: @pet
+      else
+        render json: @pet.errors, status: :unprocessable_entity
       end
+    end
+
+    def destroy
+      @pet = Pet.find(params[:id])
+      @pet.destroy
+    end
+
+     def show_dogs
+      @dogs = Pet.by_species("Dog")
+      render json: @dogs
+    end
+
+    private
+
+    def pet_params
+      params.require(:pet).permit(
+        :name,
+        :age,
+        :owner,
+        :species,
+        :gender,
+        :color,
+        :date_of_birth,
+        :health_score,
+        :personality_traits,
+        :pet_status,
+        :bio,
+        :weight
+      )
+    end
+    
 
 end
